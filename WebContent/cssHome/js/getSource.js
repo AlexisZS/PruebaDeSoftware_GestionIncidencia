15388ea3275a4b54141474a222b9047e6b8fc542
AlexisZS <alexis.zs.1894@gmail.com>
2016-10-23 23:35:16 -0500

    V1

---
+++ WebContent/cssHome/js/getSource.js
@@ -0,0 +1,80 @@
+
+
+//LLAMAR AL HOME
+function irHOME() {
+	window.open('home.xhtml','_self')
+	}
+
+//LLAMAR AL LISTADO DE VIDEO
+function irVIDEOS() {
+	window.open('videos.xhtml','_self')
+	}
+//LLAMAR  NUEVO DE VIDEO
+function irNUEVOVIDEO() {
+	window.open('videoNUEVO.xhtml','_self')
+	}
+//LLAMAR AL DETALLE DE VIDEO
+function irNUEVODETALLE() {
+	window.open('videoDETALLE.xhtml','_self')}
+
+
+//LLAMAR AL LISTADO DE CLIENTES
+function irCLIENTES() {
+	window.open('clientes.xhtml','_self')
+	}
+
+//LLAMAR NUEVO DE CLIENTE
+function irCLIENTENUEVO() {
+	window.open('clienteNUEVO.xhtml','_self')
+	}
+
+//LLAMAR AL DETALLE DEL CLIENTE
+function irCLIENTEDETALLE() {
+	window.open('clienteDETALLE.xhtml','_self')
+	}
+
+//LLAMAR AL LISTADO DE CLIENTES
+function irUSUARIOS() {
+	window.open('usuarios.xhtml','_self')
+	}
+
+//LLAMAR NUEVO DE CLIENTE
+function irUSUARIONUEVO() {
+	window.open('usuarioNUEVO.xhtml','_self')
+	}
+
+//LLAMAR AL DETALLE DEL CLIENTE
+function irUSUARIODETALLE() {
+	window.open('usuarioDETALLE.xhtml','_self')
+	}
+
+
+
+//HELP
+function help() {
+	window.open('help.xhtml','_self')
+	}
+
+
+//SALIR
+function salir() {
+	window.open('loginIntranet.xhtml','_self')
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
