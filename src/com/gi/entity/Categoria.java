4a661fd85c7d0584b4abbebe5b59bb5ac77241e5
AlexisZS <alexis.zs.1894@gmail.com>
2016-10-23 23:33:14 -0500

    V1

--- src/com/gi/entity/Categoria.java
+++ src/com/gi/entity/Categoria.java
@@ -13,11 +13,32 @@ import javax.persistence.*;
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	
 	@Id
 	@Column(name="COD_CAT")
 	private int codCat;
 
 	@Column(name="DES_CAT")
 	private String desCat;
+
+	public int getCodCat() {
+		return codCat;
+	}
+
+	public void setCodCat(int codCat) {
+		this.codCat = codCat;
+	}
+
+	public String getDesCat() {
+		return desCat;
+	}
+
+	public void setDesCat(String desCat) {
+		this.desCat = desCat;
+	}
+	
+	
+	
+	
 
 }
